b0bfe9c0cd8bac0d6d5c84d3b45ef4c28722176f
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2024-11-13 20:44:17 -0300

    feat: added user input

--- quarenta-tres.c
+++ quarenta-tres.c
@@ -9,6 +9,15 @@ int pow_number(int number, int pow) {
 }
 
 int main() {
-    printf("%d\n", pow_number(2, 10));
+    int number;
+    int expoent;
+
+    printf("Write a number: ");
+    scanf("%d", &number);
+
+    printf("Write the expoent: ");
+    scanf("%d", &expoent);
+
+    printf("%d^%d = %d\n", number, expoent, pow_number(number, expoent));
     return 0;
 }
